6cd504c1a5a552e8703e05928fcc7b85dd091057
anubrata07 <anubrata2001.choudhury@gmail.com>
2022-07-02 17:41:00 +0530

    DiceRoller App

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewDicy;
+    private Random myRandomNumber = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageViewDicy = findViewById(R.id.imageView);
+
+        imageViewDicy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollOurDice();
+            }
+        });
+    }
+
+    private void rollOurDice(){
+        int myNumber= myRandomNumber.nextInt(6)+1;
+
+        switch (myNumber){
+            case 1:
+                imageViewDicy.setImageResource(R.drawable.dice_1);
+                break;
+            case 2:
+                imageViewDicy.setImageResource(R.drawable.dice_2);
+                break;
+            case 3:
+                imageViewDicy.setImageResource(R.drawable.dice_3);
+                break;
+            case 4:
+                imageViewDicy.setImageResource(R.drawable.dice_4);
+                break;
+            case 5:
+                imageViewDicy.setImageResource(R.drawable.dice_5);
+                break;
+            case 6:
+                imageViewDicy.setImageResource(R.drawable.dice_6);
+                break;
+
+        }
+    }
+}
